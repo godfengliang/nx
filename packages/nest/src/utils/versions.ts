5db103bde5de6da25d8824265d14dce32ead2699
Jason Jean <jasonjean1993@gmail.com>
2026-05-24 10:28:37 -0500

    fix(nest)!: migrate @nx/nest to local dist build

--- packages/nest/src/utils/versions.ts
+++ packages/nest/src/utils/versions.ts
@@ -1,4 +1,5 @@
-export const nxVersion = require('../../package.json').version;
+import { join } from 'path';
+export const nxVersion = require(join('@nx/nest', 'package.json')).version;
 
 export const nestJsVersion = '^11.0.0';
 export const nestJsSchematicsVersion = '^11.0.0';
