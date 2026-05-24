12b56cab0e8cda43487a582a99a9b944e5200ffd
Jason Jean <jasonjean1993@gmail.com>
2026-05-24 10:30:09 -0500

    fix(bundling)!: migrate @nx/rollup to local dist build

--- packages/rollup/src/utils/versions.ts
+++ packages/rollup/src/utils/versions.ts
@@ -1,4 +1,5 @@
-export const nxVersion = require('../../package.json').version;
+import { join } from 'path';
+export const nxVersion = require(join('@nx/rollup', 'package.json')).version;
 export const coreJsVersion = '^3.36.1';
 export const rollupVersion = '^4.14.0';
 export const swcLoaderVersion = '0.1.15';
