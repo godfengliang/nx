5d399b15d08ef9589eceba5487a22f2dd1f86526
Jason Jean <jasonjean1993@gmail.com>
2026-05-24 10:27:18 -0500

    fix(webpack)!: migrate @nx/webpack to local dist build

--- packages/webpack/src/utils/versions.ts
+++ packages/webpack/src/utils/versions.ts
@@ -1,4 +1,5 @@
-export const nxVersion = require('../../package.json').version;
+import { join } from 'path';
+export const nxVersion = require(join('@nx/webpack', 'package.json')).version;
 
 export const swcLoaderVersion = '0.1.15';
 export const tsLibVersion = '^2.3.0';
