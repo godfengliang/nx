589ec66c0882dd479d9c1f84b8b1b9587045209f
Jason Jean <jasonjean1993@gmail.com>
2026-05-24 10:28:38 -0500

    fix(storybook)!: migrate @nx/storybook to local dist build

--- packages/storybook/src/utils/versions.ts
+++ packages/storybook/src/utils/versions.ts
@@ -1,4 +1,5 @@
-export const nxVersion = require('../../package.json').version;
+import { join } from 'path';
+export const nxVersion = require(join('@nx/storybook', 'package.json')).version;
 export const litVersion = '^2.6.1';
 export const tsNodeVersion = '10.9.1';
 export const tsLibVersion = '^2.3.0';
